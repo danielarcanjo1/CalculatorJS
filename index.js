0fe155d6ed3bede90d465fa05a4aebf064a11d70
Daniel <danielarcanjo1@gmail.com>
2023-07-05 18:39:08 -0300

    comecando calculadora

--- index.js
+++ index.js
@@ -2,22 +2,46 @@ function calculatorCreate(){
     return{
         display: document.querySelector('.displayCalc'),
         buttons: Array.from(document.querySelectorAll('.button')),
+        conta: 0,
 
-        incia(){
+        startCalculator(){
             this.clickButons()
         },
 
         clickButons(){
-        this.buttons.forEach(function(button){
-            button.addEventListener('click',function(){
-                console.log(button.textContent)
-                //this.display.value = button.textContent
+        this.buttons.forEach((button) =>{
+            button.addEventListener('click',() =>{
+                if(button.classList.contains('buttonNumber')){
+                    this.display.value += button.textContent
+                }
+                
+                if(button.classList.contains('buttonClear')){
+                    this.clearDisplay();
+                }
+                
+                if(button.classList.contains('buttonDel')){
+                    this.erase();
+                }
+
+                //let conta = 0 
+                //conta += button.textContent
             })
         })
 
+        },
+
+        clearDisplay(){
+            this.display.value = ' '
+        },
+        erase(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        
+        realizaConta(){
+            
         }
     }
 }
 
 const calculator = calculatorCreate();
-calculator.incia();
+calculator.startCalculator();
