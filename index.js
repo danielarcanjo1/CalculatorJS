5bf5fc4a6e9a620fb1e775e5334928cc3cc4f8f6
Daniel <danielarcanjo1@gmail.com>
2023-07-05 19:40:14 -0300

    atualização de funcionalidade

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ function calculatorCreate(){
     return{
         display: document.querySelector('.displayCalc'),
         buttons: Array.from(document.querySelectorAll('.button')),
-        conta: 0,
 
         startCalculator(){
             this.clickButons()
+            this.pressBackSpace()
+            this.pressEnter()
         },
 
         clickButons(){
         this.buttons.forEach((button) =>{
             button.addEventListener('click',() =>{
-                if(button.classList.contains('buttonNumber')){
+                if(button.classList.contains('buttonNumber') || 
+                button.classList.contains('buttonPlus') || 
+                button.classList.contains('buttonMinus') || 
+                button.classList.contains('buttonDiv') ||
+                button.classList.contains('buttonMult') ){
                     this.display.value += button.textContent
                 }
                 
@@ -23,22 +28,55 @@ function calculatorCreate(){
                     this.erase();
                 }
 
-                //let conta = 0 
-                //conta += button.textContent
+                if(button.classList.contains('buttonEqual')){
+                    this.calculate();
+                }
+        
             })
         })
 
         },
-
+        pressBackSpace() {
+            this.display.addEventListener('keydown', e => {
+              if (e.keyCode === 8) {
+                e.preventDefault();
+                this.clearDisplay();
+              }
+            });
+          },
+      
+          pressEnter() {
+            this.display.addEventListener('keyup', e => {
+              if (e.keyCode === 13) {
+                this.realizaConta();
+              }
+            });
+          },
+      
         clearDisplay(){
             this.display.value = ' '
         },
+
         erase(){
             this.display.value = this.display.value.slice(0, -1);
         },
-        
-        realizaConta(){
-            
+
+        calculate(){
+            let conta = this.display.value;
+            console.log(conta)
+            try {
+                conta = eval(conta);
+
+                if(!conta) {
+                alert('Conta inválida');
+                return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta inválida');
+                return;
+            }
         }
     }
 }
